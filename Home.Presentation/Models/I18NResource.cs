c1773a50b6262185fa3156c3e000918370935940
An Nguyen <annguyen.it.utc@gmail.com>
2024-07-13 03:25:32 +0700

    chore: ignore naming rule for I18NResource

--- Home.Presentation/Models/I18NResource.cs
+++ Home.Presentation/Models/I18NResource.cs
@@ -1,3 +1,5 @@
 ﻿namespace Home.Presentation;
 
+#pragma warning disable S101
 public class I18NResource;
+#pragma warning restore S101
--- IdentityProvider/Models/I18NResource.cs
+++ IdentityProvider/Models/I18NResource.cs
@@ -1,3 +1,5 @@
 ﻿namespace IdentityProvider;
 
+#pragma warning disable S101
 public class I18NResource;
+#pragma warning restore S101
