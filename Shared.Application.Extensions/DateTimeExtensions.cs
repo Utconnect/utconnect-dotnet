2266a5f9c4166ebddf33f32c3230624b443bca7b
An Nguyen <annguyen.it.utc@gmail.com>
2024-07-13 03:50:18 +0700

    refactor: Use `DateTime.UnixEpoch` instead of creating `DateTime` instances that point to the unix epoch time

--- Shared.Application.Extensions/DateTimeExtensions.cs
+++ Shared.Application.Extensions/DateTimeExtensions.cs
@@ -2,11 +2,9 @@
 
 public static class DateTimeExtensions
 {
-    private static readonly DateTime Epoch = new(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
-
     public static long ToTimestamp(this DateTime dateTime)
     {
-        TimeSpan elapsedTime = dateTime - Epoch;
+        TimeSpan elapsedTime = dateTime - DateTime.UnixEpoch;
         return (long)elapsedTime.TotalSeconds;
     }
 }
