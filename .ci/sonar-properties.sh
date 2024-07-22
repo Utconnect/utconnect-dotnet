015880579f60222aae43a7c78c63bf63cbaeab42
An Nguyen <annguyen.it.utc@gmail.com>
2024-07-22 12:55:45 +0700

    ci: update sonar version when release

---
+++ .ci/sonar-properties.sh
@@ -0,0 +1,14 @@
+﻿#!/usr/bin/env bash
+
+NEW_VERSION=$(npx standard-version --dry-run | grep 'tagging release' | tail -n1 | awk '{print $4}')
+SONAR_CLOUD_FILE=.sonarcloud.properties
+
+cp .ci/$SONAR_CLOUD_FILE.tmp $SONAR_CLOUD_FILE
+
+if [[ -n "$NEW_VERSION" ]]; then
+  echo -e "sonar.projectVersion=$NEW_VERSION" >> $SONAR_CLOUD_FILE
+else
+  echo -e "sonar.projectVersion=latest" >> $SONAR_CLOUD_FILE
+fi
+
+git add $SONAR_CLOUD_FILE
